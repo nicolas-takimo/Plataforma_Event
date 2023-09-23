2a05024178d4cf584229881451dd0037b1ee742b
darknessblock <enzo.bondan@hotmail.com>
2023-09-23 17:05:46 -0400

    Atualizado faltando 10min 2 aula

--- event-plataform/src/Components/Lesson.tsx
+++ event-plataform/src/Components/Lesson.tsx
@@ -1,7 +1,39 @@
-export function Lesson (){
+import { CheckCircle, Lock} from 'phosphor-react'
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+
+export function Lesson (props: LessonProps){
+    const isLessonAvailable = false;
     return (
         <a href="#">
-        <span>Quarta • 23 de junho • 19h00</span>
+            <span className="text-gray-300">{props.availableAt.toString()}</span>
+            <div className="rounded border border-gray-500 p-4 mt-2">
+                <header className="flex items-center justify-between">    
+                    {isLessonAvailable ? (
+                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                            <CheckCircle size={20} />
+                            Conteúdo Liberado
+                        </span>
+                    ): (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                            <Lock size={20} />
+                        Em Breve
+                    </span>
+                    )}
+                    <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
+                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+                    </span>
+                </header>
+                <strong className="text-gray-200 mt-5 block">
+                    {props.title}
+                </strong>   
+            </div>
         </a>
     )
 }
--- event-plataform/src/lib/apollo.ts
+++ event-plataform/src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.hygraph.com/v2/cll7lqkv10lbq01ughjoz14dw/master',
+    uri: 'https://api-sa-east-1.hygraph.com/v2/cll7lows70mle01ud3l7s4ad0/master',
     cache: new InMemoryCache()
 })
